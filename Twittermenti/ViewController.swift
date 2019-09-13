0578ad4250ca8e4193f5f02c3b88ab8ec02dd1f8
Destiny Sopha <destiny@sopha.org>
2019-09-13 07:07:29 -0400

    Initial Commit, nothing really much here yet.

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//  Based on the original app created by Angela Yu on 07/17/2018.
+//
+//  Modified by Destiny Sopha starting on 09/13/2019.
+//  Copyright © 2019 Over The Moon Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
