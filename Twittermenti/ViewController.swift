f8328c618639bf88c2b346f7875a080683e94305
Destiny Sopha <destiny@sopha.org>
2019-09-16 19:41:14 -0400

    Works on simulator, keyboard covers input box on physical device though - must be a constraints issue

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -14,45 +14,103 @@
 //
 
 import UIKit
-// import Foundation
+import Foundation // Required for the switch statement i guess
 import SwifteriOS
 import CoreML
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var sentimentLabel: UILabel!
-
-    let sentimentClassifier = TweetSentimentClassifier()
   
-    let tweetsCount = 100 // This is how many tweets the app will retreive (maximum allowed for free is 100)
+  @IBOutlet weak var backgroundView: UIView!
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var sentimentLabel: UILabel!
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  let sentimentClassifier = TweetSentimentClassifier()
+  
+  let tweetsCount = 100 // This is how many tweets the app will retreive (maximum allowed for free is 100)
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  @IBAction func predictPressed(_ sender: Any) {
+     FetchTweets()
+  }
 
-      // When we start moving stuff, I thinik we should leave teh next three lines here //
-      let apiKeyID = valueForAPIKey(named:"Twitter_API_Key")
-      let apiSecretKeyID = valueForAPIKey(named:"Twitter_API_SecretKey")
-      let swifter = Swifter(consumerKey: apiKeyID, consumerSecret: apiSecretKeyID)
+  
+  func FetchTweets() {
+    
+    let apiKeyID = valueForAPIKey(named:"Twitter_API_Key")
+    let apiSecretKeyID = valueForAPIKey(named:"Twitter_API_SecretKey")
+    let swifter = Swifter(consumerKey: apiKeyID, consumerSecret: apiSecretKeyID)
+    
+    if let searchText = textField.text {
       
-      let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company!")
-      print(prediction.label)
+      swifter.searchTweet(using: searchText, lang: "en", count: tweetsCount, tweetMode: .extended, success: { (results, metadata) in
+        
+        var tweets = [TweetSentimentClassifierInput]()
+        
+        for i in 0..<self.tweetsCount {
+          if let tweet = results[i]["full_text"].string {
+            let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+            tweets.append(tweetForClassification)
+          }
+          
+        }
+        
+        self.makePrediction(with: tweets)
 
-      swifter.searchTweet(using: "@Apple", lang: "en", count: tweetsCount, tweetMode: .extended, success: { (results, metadata) in
-        // print(results) // prints the JSAON of the past 100 tweets
       }) { (error) in
         print("There was an error with the Twitter API request, \(error)")
       }
       
     }
 
-    @IBAction func predictPressed(_ sender: Any) {
-    
-    }
+  }
+
   
-  func FetchTweets() {
+  func makePrediction(with tweets: [TweetSentimentClassifierInput]) {
     
+    do {
+      
+      let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+      
+      var sentimentScore = 0
+      
+      for pred in predictions {
+        let sentiment = pred.label
+        
+        if sentiment == "Pos" {
+          sentimentScore += 1
+        } else if sentiment == "Neg" {
+          sentimentScore -= 1
+        }
+        
+      }
+      
+      updateUI(with: sentimentScore)
+      
+    } catch {
+      print("There was an error with making a prediction, \(error)")
+    }
+
+  }
+
+  
+  func updateUI(with sentimentScore: Int) {
+    
+    switch sentimentScore {
+      case 21 ... self.tweetsCount: self.sentimentLabel.text = "😍"
+      case 11 ... 20: self.sentimentLabel.text = "😀"
+      case 1 ... 10: self.sentimentLabel.text = "🙂"
+      case 0: self.sentimentLabel.text = "😐"
+      case -9 ... -1: self.sentimentLabel.text = "🙁"
+      case -20 ... -10: self.sentimentLabel.text = "😡"
+      default: self.sentimentLabel.text = "🤮"
+    }
+
   }
 
 }
